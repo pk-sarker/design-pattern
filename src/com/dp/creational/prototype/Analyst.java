cc1b58752eb1369e068fca29170902e7c5b86a3a
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2018-07-18 07:24:22 -0400

    Added Analyst class for prototype design pattern.

---
+++ src/com/dp/creational/prototype/Analyst.java
@@ -0,0 +1,25 @@
+package com.dp.creational.prototype;
+
+public class Analyst extends Employee {
+
+    Analyst() {
+        this.setDesignation("Analyst");
+    }
+
+    @Override
+    public void printEmployeeRecord() {
+        System.out.println("\n\n------- Employee -------");
+        System.out.println("ID: "+this.getId());
+        System.out.println("Name: "+this.getName());
+        System.out.println("Designation: "+this.getDesignation());
+        System.out.println("Experience: "+this.getExperience());
+    }
+
+    @Override
+    public void addEmployee(String id, String name, int experience) {
+        this.setId(id);
+        this.setName(name);
+        this.setExperience(experience);
+
+    }
+}
