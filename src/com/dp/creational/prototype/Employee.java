16c363360c447b4da12255439e63d5a7ec99b621
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2018-07-18 07:21:27 -0400

    Added employee abstract class for prototype design pattern.

---
+++ src/com/dp/creational/prototype/Employee.java
@@ -0,0 +1,56 @@
+package com.dp.creational.prototype;
+
+public abstract class Employee implements Cloneable {
+    private String id;
+    private String name;
+    private String designation;
+    private int experience;
+
+    abstract void printEmployeeRecord();
+    abstract void addEmployee(String id, String name, int experience);
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public Object clone() {
+        Object clone = null;
+
+        try {
+            clone = super.clone();
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return clone;
+    }
+}
