688d047c5f963e4955704c2e2fe535d03322aeaa
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2018-07-19 07:50:28 -0400

    console logger class created

---
+++ src/com/dp/behavioral/chain/ConsoleLogger.java
@@ -0,0 +1,12 @@
+package com.dp.behavioral.chain;
+
+public class ConsoleLogger extends AbstractLogger {
+    public ConsoleLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
